4d642bb820cfd5807d4a4bb1e3a775efa0c42b7b
Irene Alvarado <irealva@github.com>
2021-04-28 11:35:11 -0700

    Add diagram

--- query.sql
+++
@@ -1,9 +0,0 @@
-SELECT callsign,
-       number,
-       origin,
-       ( lastseen - firstseen ) AS duration,
-       day
-FROM   flights
-WHERE  destination = 'KSFO'
-ORDER  BY day DESC
-LIMIT  1000;
